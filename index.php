1128bec0ef48f26d710cc6ea52045c9433fb17ef
Peter Pedersen <petnped@gmail.com>
2015-06-14 10:33:13 +0200

    index.php now points at home.php

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
   <head>
   <meta name="author" content="Peter Pedersen">
   <link rel="stylesheet" type="text/css" href="style.css">
-  <title>Velkommen til Borgmestergaarden</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1;url=http://example.com">
+        <script type="text/javascript">
+            window.location.href = "home.php"
+        </script>
+        <title>Page Redirection</title>
 
   </head>
   <body>
-      <h1>Velkommen til Borgmestergaardens (bruger) hjemmeside</h1>
-<h2>Siden er under opbygning...</h2>
-<h3>For at komme til admin siden <a href="admin/index.php">tryk her</a>.</h3>
+      <h1>Velkommen til Borgmestergaardens hjemmeside</h1>
+      <h2>For at komme til brugesiden</h2>
+      <h3>Tryk <a href="home.php">her</a>.</h3>
+      <h2>For at komme til Admin siden</h2>
+      <h3>Tryk <a href="admin/index.php">her</a>.</h3>
   </body>
 </html>
